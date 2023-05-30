edae1935802c2764d815205e6d5d4c6d090f071e
Arkadiusz Matlak <piccolo@poczta.fm>
2023-05-30 11:07:56 +0200

    promise - using kebab example, remember kebab joke (krakow, kazimierz, 3  at night, gastro

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
-const box = document.querySelector('.box');
+// const box = document.querySelector('.box');
+//
+// // callback hell
+// setTimeout(() => {
+//     box.style.left = "500px";
+//     setTimeout(() => {
+//         box.style.top = "500px";
+//         setTimeout(() => {
+//             box.style.left = "0px";
+//             setTimeout(() => {
+//                 box.style.top = "0px";
+//             }, 3000);
+//         }, 3000);
+//     }, 3000);
+// }, 3000);
 
-// callback hell
-setTimeout(() => {
-    box.style.left = "500px";
+const receipt = new Promise((resolve, reject) => {
     setTimeout(() => {
-        box.style.top = "500px";
-        setTimeout(() => {
-            box.style.left = "0px";
-            setTimeout(() => {
-                box.style.top = "0px";
-            }, 3000);
-        }, 3000);
+        resolve('Kebab maj friend!');
+    }, 3001);
+
+    setTimeout(() => {
+        reject('No, kebab, łonna fajt?!');
     }, 3000);
-}, 3000);
+});
 
+console.log(receipt);
 
+receipt
+    .then((msg) => {
+        console.log(msg)
+    })
+    .catch((msg) => {
+        console.log(msg)
+    })
